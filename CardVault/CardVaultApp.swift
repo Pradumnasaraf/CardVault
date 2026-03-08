2a6634195c4883f3d6dffe3ba6c2356903ac7276
Pradumna Saraf <pradumnasaraf@gmail.com>
2026-03-08 15:48:56 +0530

    Initial Commit

---
+++ CardVault/CardVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardVaultApp.swift
+//  CardVault
+//
+//  Created by Pradumna Saraf on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CardVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
